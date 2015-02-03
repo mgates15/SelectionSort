05f61432687405a3dbfd2704e630173f45e23f78
mgates15 <mgates15@groton.org>
2015-02-03 12:52:31 -0500

    Create Reader.java

---
+++ Reader.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Reader {
+	public static int[] read(){
+		String fileName = "MichaelGatesArray.txt";
+		File file = new File(fileName);
+		Scanner input = null;
+		try
+		{
+			input = new Scanner(file);
+		}
+		catch (FileNotFoundException ex)
+		{
+		System.out.println();
+		}
+		int[] num = new int[250];
+		
+		for(int i = 0; i<250; i++){
+		num[i] = input.nextInt();
+			}
+		
+		return num;}
+}
